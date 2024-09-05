32575b3c1b1edd1259ab6d7bba2d40957572692f
tamimiqbalfysal <tamimiqbal.fysal@gmail.com>
2024-09-05 19:07:27 +0600

    It is in the Facebook era

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: ''),
